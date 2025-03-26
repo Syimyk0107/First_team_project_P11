2dda4ce400d68cc6fe72eba256d3b133d31825ad
Syimyk0107 <syimykkubandykov01@gmail.com>
2025-03-26 20:26:17 +0000

    First team project_P11

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Добро пожаловать в нашу команду!!! Удачи!")
+
+# PS C:\Users\Notnik_kg\Desktop\First_team_project> git pull origin main
+# PS C:\Users\Notnik_kg\Desktop\First_team_project> git merge almaz
+# PS C:\Users\Notnik_kg\Desktop\First_team_project> git add .
+# PS C:\Users\Notnik_kg\Desktop\First_team_project> git commit -m "Almazs"
+# PS C:\Users\Notnik_kg\Desktop\First_team_project> git push origin main
